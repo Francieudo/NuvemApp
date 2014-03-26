d61eff22d36c12f27e83c39116f60973ee1d6574
francieudo <francieudocosta@gmail.com>
2014-03-26 00:16:37 -0300

    adicionado configurações de deploy azure

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,7 +2,7 @@ worker_processes 2
 timeout 30
 listen "/tmp/unicorn.blog.sock"
  
-root = "/home/francieudo/apps/NuvemApp/current"
+root = "/var/www/NuvemApp/current"
  
 working_directory root
  
--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -4,10 +4,10 @@ set -e
  
 # Feel free to change any of the following variables for your app:
 TIMEOUT=${TIMEOUT-60}
-APP_ROOT=/home/francieudo/apps/NuvemApp/current
+APP_ROOT=/var/www/NuvemApp/current
 PID=$APP_ROOT/tmp/pids/unicorn.pid
 CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
-AS_USER=blogger
+AS_USER=francieudo
 set -u
  
 OLD_PIN="$PID.oldbin"
